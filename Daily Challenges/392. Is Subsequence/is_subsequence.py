3c858bc592295c4e4fffb8eb46e7c1ccc84c6441
unknown <jmuribef@eafit.edu.co>
2023-09-21 21:51:48 -0500

    add daily sep 22

---
+++ Daily Challenges/392. Is Subsequence/is_subsequence.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if len(s)>len(t):
+            return False
+        if not s:
+            return True
+        j = 0 
+        for i in range(len(t)):
+            if t[i]==s[j]:
+                j+=1
+            if j == len(s):
+                return True
+        return False
